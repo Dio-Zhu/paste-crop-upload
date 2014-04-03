f19b21f3165e483ee881109d65d3e65a9d26aefa
lj <lj@lj-u1294large.(none)>
2014-04-03 16:02:49 +0800

    添加config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * config of paste, crop and upload image
+ * gipsaliu@gmail.com
+ * since 2014-04-03
+ *
+ */
+
+$_cfg_tmp_file_path = './uploads/tmp/';
+$_cfg_final_file_path = './uploads/final/';
+?>
